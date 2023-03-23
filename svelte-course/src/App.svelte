907eb625e2d18d9b8e0ef86b210fbbd75e0c9deb
EKI-INDRADI <eki.indradi@gmail.com>
2023-03-24 02:16:51 +0700

    component props

--- svelte-course/src/App.svelte
+++ svelte-course/src/App.svelte
@@ -1,10 +1,18 @@
 <script>
 	// let name = 'Svelete';
-    // let src="http://picsum.com/200/200" //example random photo
-    // let string ='some <string>string</string> with html.'
+	// let src="http://picsum.com/200/200" //example random photo
+	// let string ='some <string>string</string> with html.'
+
+	import Counter from './lib/Counter.svelte';
+
+	// const maxCount = 6;
+
+	const props = {
+		maxCount : 10,
+		initialCount: 5
+	}
 
 
-	import Counter from './lib/Counter.svelte'
 </script>
 
 <!-- <img src="http://picsum.com/200/200"/>  example random photo -->
@@ -16,20 +24,22 @@
 <!-- <img {src} alt={`Some Text ${name}`}/> -->
 <!-- <p>{string}</p> html not work -->
 
-
 <!-- <img {src} alt="Some Text {name}"/>
 <h1>Hello {name.toLowerCase()}!</h1>
 <p>{@html string}</p> -->
 
+<!-- <Counter /> -->
 
-<Counter />
+<!--- --------------------------------- COMPONENT PROPS-->
+<!-- <Counter initialCount={3} maxCount={10} />  -->
+<!-- <Counter initialCount={3} />   -->
+<!-- <Counter initialCount={3} {maxCount} /> -->
+<Counter {...props}/>
 
 
+<!--- --------------------------------- COMPONENT PROPS-->
 <style>
 	/* h1 {
 		color: red;
 	} */
-
-
-
 </style>
