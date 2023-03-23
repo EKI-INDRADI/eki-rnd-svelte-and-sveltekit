114f8e6b12b89d5b1c09785a6e0948128e8c41ac
EKI-INDRADI <eki.indradi@gmail.com>
2023-03-24 02:12:59 +0700

    Revert "Component Props"
    This reverts commit be442326f9f4cf687f4e831cf88c4cc7f21787fa.

--- svelte-course/src/App.svelte
+++ svelte-course/src/App.svelte
@@ -1,18 +1,10 @@
 <script>
 	// let name = 'Svelete';
-	// let src="http://picsum.com/200/200" //example random photo
-	// let string ='some <string>string</string> with html.'
-
-	import Counter from './lib/Counter.svelte';
-
-	// const maxCount = 6;
-
-	const props = {
-		maxCount : 10,
-		initialCount: 5
-	}
+    // let src="http://picsum.com/200/200" //example random photo
+    // let string ='some <string>string</string> with html.'
 
 
+	import Counter from './lib/Counter.svelte'
 </script>
 
 <!-- <img src="http://picsum.com/200/200"/>  example random photo -->
@@ -24,22 +16,20 @@
 <!-- <img {src} alt={`Some Text ${name}`}/> -->
 <!-- <p>{string}</p> html not work -->
 
+
 <!-- <img {src} alt="Some Text {name}"/>
 <h1>Hello {name.toLowerCase()}!</h1>
 <p>{@html string}</p> -->
 
-<!-- <Counter /> -->
 
-<!--- --------------------------------- COMPONENT PROPS-->
-<!-- <Counter initialCount={3} maxCount={10} />  -->
-<!-- <Counter initialCount={3} />   -->
-<!-- <Counter initialCount={3} {maxCount} /> -->
-<Counter {...props}/>
+<Counter />
 
 
-<!--- --------------------------------- COMPONENT PROPS-->
 <style>
 	/* h1 {
 		color: red;
 	} */
+
+
+
 </style>
--- svelte-course/src/lib/Counter.svelte
+++ svelte-course/src/lib/Counter.svelte
@@ -1,20 +1,11 @@
 <!-- example counter -->
 <script>
-	export let initialCount = 0;
-	// export let maxCount;
-	// export let maxCount = 10;
-	export let maxCount = undefined
-
-  console.log($$props, $$restProps) // untuk console.log object props yang ada di app.svelte
-
 	// let count = 0;
-	// let count1 = 0;
-	// let count2 = 0;
-
-	let count = initialCount;
+	let count1 = 0;
+	let count2 = 0;
 
 	// let string = `You have clicked ${count} times.`; // NOT REACTIVE
-	// let string;
+	let string;
 
 	//-------------------- REACTIVE STATEMENT = buat yg not reactive jadi reactive
 	// $: {
@@ -46,15 +37,20 @@
 
 	//-----------------FUNCTION WITH REACTIVE
 
-	//------------------------- REACTIVE FUNCTION
 
-	// function setCount2(x) {
-	//   count2 = x;
-	// }
-	// $: setCount2(count1);
-	// $: string = `Count2 is ${count2}`;
 
-	//------------------------- REACTIVE FUNCTION
+
+
+  //------------------------- REACTIVE FUNCTION
+
+  function setCount2(x) {
+    count2 = x;
+  }
+  $: setCount2(count1);
+	$: string = `Count2 is ${count2}`; 
+
+//------------------------- REACTIVE FUNCTION
+
 
 	// $: if (count > 5) { // by condition
 	// 	console.log(`the count is ${count}`);
@@ -66,9 +62,9 @@
 	// 	count += 1;
 	// }
 
-	// function increment1() {
-	// 	count1 += 1;
-	// }
+	function increment1() {
+		count1 += 1;
+	}
 
 	// function increment2() {
 	// 	count2 += 1;
@@ -77,19 +73,13 @@
 	// const increment = () => {
 	//   count += 1
 	// }
-
-	function increment() {
-		if (count == maxCount) return;
-		count += 1;
-	}
 </script>
 
 <!-- <button on:click={increment}>Clicks {count}</button> -->
-<!-- <button on:click={increment1}>Clicks {count1}</button> -->
+<button on:click={increment1}>Clicks {count1}</button>
 <!-- <button on:click={increment2}>Clicks {count2}</button> -->
-<button on:click={increment}>Clicks {count}</button>
 
-<!-- <h3>{string}</h3> -->
+<h3>{string}</h3>
 <!-- NOT REACTIVE jika sudah buat statement reactive akan jadi REACTIVE-->
 <!-- <h3>You have clicked {count} times.</h3> REACTIVE -->
 
