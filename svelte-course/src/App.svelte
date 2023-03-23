73360a5041fb0388ef622b6547b4048446b0fd5c
EKI-INDRADI <eki.indradi@gmail.com>
2023-03-23 10:03:43 +0700

    OK

---
+++ svelte-course/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	let name = 'Svelete';
+    let src="http://picsum.com/200/200" //example random photo
+    let string ='some <string>string</string> with html.'
+</script>
+
+<!-- <img src="http://picsum.com/200/200"/>  example random photo -->
+<!-- == -->
+<!-- <img src={src}/> -->
+<!-- == -->
+<!-- <img {src} alt="Some Text {name}"/> -->
+<!-- == -->
+<!-- <img {src} alt={`Some Text ${name}`}/> -->
+<img {src} alt="Some Text {name}"/>
+<h1>Hello {name.toLowerCase()}!</h1>
+<!-- <p>{string}</p> html not work -->
+<p>{@html string}</p>
+
+
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
