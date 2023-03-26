bd6fd855da3f698f67f4651e3c4218a526ba3b2f
EKI-INDRADI <eki.indradi@gmail.com>
2023-03-26 21:56:54 +0700

    update

--- svelte-course/src/App.svelte
+++ svelte-course/src/App.svelte
@@ -9,7 +9,12 @@
 <!--<Button >Button Text</Button>-->  <!--// Button Text = children -->
 <!-- <Button/> -->
 
-<Button >Button Text</Button>
+<!-- <Button >Button Text</Button> -->
+
+
+<!-- eki-rnd-svelte-and-sveltekit\svelte-course\src\lib\Button.svelte -->
+<Button size="small" shadow>Button Text</Button>
+<!-- <Button size="large" shadow>Button Text</Button> -->
 
 <style>
 
